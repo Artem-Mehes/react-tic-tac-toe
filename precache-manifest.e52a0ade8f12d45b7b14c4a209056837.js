9c50f8362b954c1d39e9a513d58a45e5141d8714
Artem Meges <artemmeges@gmail.com>
2020-08-06 17:46:11 +0300

    Updates

---
+++ precache-manifest.e52a0ade8f12d45b7b14c4a209056837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6519f01c7519bf3bf3b6cf1a3b331b",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "519e3fa96a4407a94e85",
+    "url": "/react-tic-tac-toe/static/css/main.9a167a6a.chunk.css"
+  },
+  {
+    "revision": "2ef94786d902ceba7771",
+    "url": "/react-tic-tac-toe/static/js/2.a272cdaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.a272cdaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519e3fa96a4407a94e85",
+    "url": "/react-tic-tac-toe/static/js/main.a1d4f3db.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  }
+]);
